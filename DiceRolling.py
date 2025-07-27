324c2cb26faea03923c05727de54b081a567f095
arasriver <aras.saeedian@gmail.com>
2025-07-27 14:36:42 +0200

    smalle update

--- DiceRolling.py
+++ DiceRolling.py
@@ -1,14 +1,19 @@
 import random
 
+times = int(input("How many dice do you want to roll? "))
 dice = input("Roll the dice? (y/n): ").lower()
+counter = 0
 while dice != "n":
     if dice == "y":
-        result = (random.randint(1,6),random.randint(1,6))
-        print(result)
+        for i in range(times):
+            result = (random.randint(1,6),random.randint(1,6))
+            counter += 1
+            print(result)
     else:
         print("Invalid choice! ")
 
     dice = input('Roll the dice? (y/n): ').lower()
 
 print("Thanks for playing")
+print(f"number of dice rolling: {counter}")
 

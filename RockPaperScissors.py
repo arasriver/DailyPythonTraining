b563ae55c8a556bcd484e6a930e2341b669138b7
arasriver <aras.saeedian@gmail.com>
2025-07-28 12:09:41 +0200

    updated

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,21 +1,25 @@
 import random
+
+
 def user_input():
-    user = input("Rock, Paper, Scissors? (r/p/s) ").lower().strip()
-    return user
+    user_in = input("Rock, Paper, Scissors? (r/p/s) ").lower().strip()
+    return user_in
+
+
 def computer_input():
     items = ['r', 'p', 's']
     computer = random.choice(items)
     return computer
+
+
 def competition(user, computer):
     print(f"You chose {user}")
     print(f"Computer chose {computer}")
     if user == computer:
         print("equal")
-    elif user == "s" and computer == "p":
-        print("You won")
-    elif user == "p" and computer == "r":
-        print("You won")
-    elif user == "r" and computer == "s":
+    elif (user == "s" and computer == "p") or \
+        (user == "p" and computer == "r") or \
+        (user == "r" and computer == "s"):
         print("You won")
     else:
         print("Computer won")
@@ -27,9 +31,7 @@ while True:
     if user == 'r' or user == 'p' or user == 's':
         competition(user, comp)
         con = input("continue? (y/n): ").lower().strip()
-        if con == "y":
-            continue
-        else:
+        if con != "y":
             break
     else:
         print("Invalid choice")
@@ -37,8 +39,12 @@ while True:
 
 
 
-
-
-
-
-
+# time_round = int(input("How many round do you want to play? "))
+# for i in range(time_round):
+#     user = user_input()
+#     comp = computer_input()
+#     if user == 'r' or user == 'p' or user == 's':
+#         competition(user, comp)
+#     else:
+#         print("Invalid choice")
+#         continue

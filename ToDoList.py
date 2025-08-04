c01394848e6cb6b712a4890a4088a34411b9c69c
arasriver <aras.saeedian@gmail.com>
2025-08-04 14:26:25 +0200

    Simpel ToDo List update

--- ToDoList.py
+++ ToDoList.py
@@ -1,6 +1,24 @@
 
 todo = []
 choice = 0
+FILE_NAME = "todo.txt"
+
+
+def load_tasks():
+    try:
+        with open(FILE_NAME, "r") as file:
+            for line in file:
+                task = line.strip()
+                if task:
+                    todo.append(task)
+    except FileNotFoundError:
+        pass
+
+
+def save_tasks():
+    with open(FILE_NAME, "w") as file:
+        for task in todo:
+            file.write(task + "\n")
 
 
 def show_task():
@@ -17,25 +35,27 @@ def show_task():
 def add_task():
     task = input("Create a new task: ")
     todo.append(task)
+    save_tasks()
     print("Task added")
 
 
 def remove_task():
-    while True:
-        if not todo:
-            print("No task to remove")
+    if not todo:
+        print("No tasks in the list to remove")
+        return
+    print("List of tasks: ")
+    for i in range(len(todo)):
+        print(i + 1, ". ", todo[i])
+    try:
+        rem_task = int(input("\nEnter the task number to remove: "))
+        if 1 <= rem_task <= len(todo):
+            save_tasks()
+            print(todo[rem_task - 1], "has been removed!")
+            todo.pop(rem_task - 1)
         else:
-            if not todo:
-                print("No tasks in the list to remove")
-                break
-            else:
-                print("List of tasks: ")
-                for i in range(len(todo)):
-                    print(i + 1, ". ", todo[i])
-            rem_task = int(input("\nEnter the task number for removing: "))
-            print(todo[rem_task - 1], " has been removed!")
-            todo.remove(todo[rem_task - 1])
-            break
+            print("Invalid task number.")
+    except ValueError:
+        print("Please enter a valid number.")
 
 
 while True:
@@ -46,18 +66,16 @@ while True:
           "4. Exit \n")
     try:
         choice = int(input("Enter your choice: "))
-        if choice == 1 or choice == 2 or choice == 3 or choice == 4:
-            if choice == 1:
+        match choice:
+            case 1:
                 show_task()
-            if choice == 2:
+            case 2:
                 add_task()
-            if choice == 3:
+            case 3:
                 remove_task()
-            if choice == 4:
+            case 4:
                 print("End of program")
                 break
-        else:
-            raise ValueError
     except ValueError:
         print("Invalid choice ")
         continue

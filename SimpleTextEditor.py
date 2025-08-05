ae4241333ef0d3031fdf3fb67dff9a59f63a9820
arasriver <aras.saeedian@gmail.com>
2025-08-05 14:56:06 +0200

    Simpel ToDo List update

---
+++ SimpleTextEditor.py
@@ -0,0 +1,21 @@
+import os
+
+file_name = input("Enter the file name to open or create: ").strip().lower()
+# f = open(f"{file_name}", "w")
+if os.path.exists(file_name):
+    with open(f"{file_name}","r") as f:
+        print(f.read())
+
+
+text_content = ""
+
+while True:
+    text_content = input("Enter your text (type Save on a new line to save and exit): ")
+    if text_content != "save":
+        with open(f"{file_name}", "a") as f:
+            f.write(f"{text_content}\n")
+    else:
+        break
+
+
+print(f"{file_name} saved.")
